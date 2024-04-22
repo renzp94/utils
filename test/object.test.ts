962d5cad909a8ca733d34c535254b7dd8a57c9d4
renzp94 <1074720760@qq.com>
2024-04-22 14:36:53 +0800

    feat(object): 添加removeKey(删除对象指定属性)方法

---
+++ test/object.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'bun:test'
+import { removeKey } from '../src'
+
+test('removeKey', () => {
+  const a = { a: 1, b: 2 }
+  const b = removeKey(a, 'a')
+  expect(a).toEqual({ a: 1, b: 2 })
+  expect(b).toEqual({ b: 2 })
+})
