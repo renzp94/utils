ac08968d98c3b21a722c2104b2499d7f03784d47
renzp94 <1074720760@qq.com>
2024-04-24 15:51:13 +0800

    docs: 修复editLink错误的问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -115,7 +115,7 @@ export default defineConfig({
       },
     ],
     editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern: 'https://github.com/renzp94/utils/edit/main/docs/:path',
       text: '在 GitHub 上编辑此页面',
     },
     lastUpdated: {
