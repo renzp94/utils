7005cc01438f939b2f8fe59ccc790ff2488dd278
renzp94 <1074720760@qq.com>
2024-05-30 17:20:12 +0800

    feat(array): 添加createArray(创建数组)方法

--- src/array.ts
+++ src/array.ts
@@ -3,6 +3,31 @@ import { isArray, isFunction, isPrimitive, isString, isUnDef } from './is'
 import { removeKey } from './object'
 import { deepClone } from './other'
 
+/**
+ * 创建数组
+ * @param length 数组长度
+ * @param value 填充元素
+ * @returns 返回创建的数组
+ *
+ * @example
+ * createArray(2); // []
+ * createArray(2).length; // 2
+ * createArray(2, 0); // [0, 0]
+ * createArray(2, (i) => i * 2); // [0, 2]
+ */
+export const createArray = <T = any>(
+  length = 0,
+  value?: T | ((index: number) => T),
+): Array<T> => {
+  return Array.from({ length }).map<T>((_: unknown, index: number) => {
+    if (isFunction(value)) {
+      return value(index) as T
+    }
+
+    return value as T
+  })
+}
+
 export type DifferenceFilter<T> =
   | keyof T
   | Array<keyof T>
--- test/array.test.ts
+++ test/array.test.ts
@@ -1,5 +1,17 @@
 import { expect, test } from 'bun:test'
-import { difference, first, flatten, last } from '../src'
+import { createArray, difference, first, flatten, last } from '../src'
+
+test('创建数组', () => {
+  expect(createArray(2)).toEqual([])
+  expect(createArray(2).length).toEqual(2)
+  expect(createArray(2, 0)).toEqual([0, 0])
+  expect(createArray(2, (i) => i * 2)).toEqual([0, 2])
+  expect(createArray(2, { a: 1 })).toEqual([{ a: 1 }, { a: 1 }])
+  expect(createArray(2, (i) => ({ a: i, b: (i + 1) * (i + 2) }))).toEqual([
+    { a: 0, b: 2 },
+    { a: 1, b: 6 },
+  ])
+})
 
 test('过滤基本数据类型', () => {
   expect(difference([1, 2, 3], [2, 3])).toEqual([1])
