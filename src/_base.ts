c4d48f5fec2a134286fd8ef8a06fdbbd5647aa3c
renzp94 <1074720760@qq.com>
2024-06-13 22:12:32 +0800

    refactor(other): 抽离公共节流防抖的公共函数

--- src/_base.ts
+++ src/_base.ts
@@ -1,4 +1,5 @@
-import { isArray, isObject } from './is'
+import { isArray, isFunction, isNull, isObject } from './is'
+import type { DebounceOptions } from './other'
 
 /**
  * 调用变量的toString
@@ -57,4 +58,91 @@ export const _po = <T extends Record<PropertyKey, any>, K extends keyof T>(
       (prev, key) => (target?.[key] ? { ...prev, [key]: target[key] } : prev),
       {} as T,
     )
+}
+
+export type _DebounceCommonFn<T> = T & {
+  flush: () => void
+  cancel: (flush?: boolean) => void
+  revokeCancel: () => void
+}
+
+export const _debounceCommon = <T extends (...args: any) => any>(
+  fn: T,
+  time: number,
+  callTiming: 'before' | 'after',
+  customFn: (
+    timer: Timer | null,
+    isCancel: boolean,
+    callback: (timer: Timer | null, enableWork: boolean) => void,
+  ) => Timer | null,
+): _DebounceCommonFn<T> => {
+  if (!isFunction(fn)) {
+    throw new Error('参数fn应该为函数')
+  }
+
+  const isBefore = callTiming === 'before'
+  let enableWork = isBefore || time < 0
+  let timer: Timer | null = null
+  let _args: Parameters<T>
+  let isCancel = false
+  let isFlush = false
+
+  const _debounce = (...args: Parameters<T>) => {
+    _args = args
+    if (enableWork) {
+      // 如果被取消防抖且防抖前执行，则执行前设置enableWork
+      if (!isCancel && isBefore) {
+        enableWork = false
+      }
+      return fn(...args)
+    }
+
+    timer = customFn(timer, isCancel, (_timer, _enableWork) => {
+      timer = _timer
+      enableWork = _enableWork
+      // 如果是节流前执行，不需要再次调用。
+      if (!isBefore && !isFlush) {
+        isFlush = false
+        _debounce(...(_args ?? []))
+      }
+    })
+  }
+
+  /**
+   * 立即执行
+   * @returns 返回执行函数返回的数据
+   */
+  _debounce.flush = () => {
+    isFlush = true
+    fn(...(_args ?? []))
+  }
+  /**
+   * 取消防抖
+   * @param flush 是否立即执行待执行任务
+   */
+  _debounce.cancel = (flush = true) => {
+    // 如果有延时器则清除
+    if (!isNull(timer)) {
+      clearTimeout(timer)
+      timer = null
+      if (flush) {
+        _debounce.flush()
+      }
+    }
+    // 允许执行
+    enableWork = true
+    // 标记已取消防抖
+    isCancel = true
+  }
+  /**
+   * 撤销取消防抖
+   */
+  _debounce.revokeCancel = () => {
+    if (isCancel) {
+      isCancel = false
+      enableWork = false
+    }
+  }
+
+  return _debounce as _DebounceCommonFn<T>
 }
--- src/other/throttle.ts
+++ src/other/throttle.ts
@@ -1,4 +1,5 @@
-import { isFunction, isNull } from '../is'
+import { _debounceCommon } from '../_base'
+import { isNull } from '../is'
 
 export interface ThrottleOptions {
   time?: number
@@ -34,71 +35,20 @@ export const throttle = <T extends (...args: any) => any>(
   time = 300,
   callTiming: 'before' | 'after' = 'after',
 ): ThrottleFn<T> => {
-  if (!isFunction(fn)) {
-    throw new Error('参数fn应该为函数')
-  }
-
-  const isBefore = callTiming === 'before'
-  let enableWork = isBefore || time < 0
-  let timer: Timer | null = null
-  let _args: Parameters<T>
-  let isCancel = false
-
-  const _throttle = (...args: Parameters<T>) => {
-    _args = args
-    if (enableWork) {
-      // 如果被取消节流且节流前执行，则执行前设置enableWork
-      if (!isCancel && isBefore) {
-        enableWork = false
+  return _debounceCommon(
+    fn,
+    time,
+    callTiming,
+    (_timer, _isCancel, callback) => {
+      let timer = _timer
+      // 如果被取消节流则不需要延时执行
+      if (!_isCancel && isNull(timer)) {
+        timer = setTimeout(() => {
+          // 执行了之后就清空变量
+          callback(null, true)
+        }, time)
       }
-      return fn(...args)
-    }
-    // 如果被取消节流则不需要延时执行
-    if (!isCancel && isNull(timer)) {
-      timer = setTimeout(() => {
-        // 执行了之后就清空变量
-        timer = null
-        enableWork = true
-        // 如果是节流前执行，不需要再次调用。
-        if (!isBefore) {
-          _throttle(..._args)
-        }
-      }, time)
-    }
-  }
-
-  /**
-   * 立即执行
-   * @returns 返回执行函数返回的数据
-   */
-  _throttle.flush = () => fn(..._args)
-  /**
-   * 取消节流
-   * @param flush 是否立即执行待执行任务
-   */
-  _throttle.cancel = (flush = true) => {
-    // 如果有延时器则清除
-    if (!isNull(timer)) {
-      clearTimeout(timer)
-      timer = null
-      if (flush) {
-        _throttle.flush()
-      }
-    }
-    // 允许执行
-    enableWork = true
-    // 标记已取消节流
-    isCancel = true
-  }
-  /**
-   * 撤销取消节流
-   */
-  _throttle.revokeCancel = () => {
-    if (isCancel) {
-      isCancel = false
-      enableWork = false
-    }
-  }
-
-  return _throttle as ThrottleFn<T>
+      return timer
+    },
+  ) as ThrottleFn<T>
 }
